6f7e869e2374dd68965d5fde3bdb3a40082a5690
Primata <lucasvinzon@gmail.com>
2023-12-14 00:44:27 -0300

    fix

--- src/pages/LandingPage/Index.tsx
+++ src/pages/LandingPage/Index.tsx
@@ -3,45 +3,45 @@ import { requestFaucetWithGlobalSigner, mevmRequestFaucet, m2RequestFaucet } fro
 import { AptosClient, FaucetClient, CoinClient } from "aptos";
 import Chain from "../../components/Chain";
 
-const RPC_URL = "https://seed-node1.movementlabs.xyz";
-const FAUCET_URL = "https://seed-node1.movementlabs.xyz";
-const MEVM_URL = "https://mevm.movementlabs.xyz/v1";
-const M2_URL = "https://sui.movementlabs.xyz/faucet";
-const faucetClient = new FaucetClient(FAUCET_URL, FAUCET_URL);
-const aptosClient = new AptosClient(RPC_URL);
+const M1_URL = "https://devnet.m1.movementlabs.xyz/v1";
+const M1_FAUCET_URL = "https://devnet.m1.movementlabs.xyz/v1";
+const MEVM_M1_URL = "https://mevm.devnet.m1.movementlabs.xyz/v1";
+const M2_URL = "https://devnet.m2.movementlabs.xyz/faucet";
+const faucetClient = new FaucetClient(M1_URL, M1_FAUCET_URL);
+const aptosClient = new AptosClient(M1_URL);
 const coinClient = new CoinClient(aptosClient);
 
 export default function LandingPage() {
 
 
-  const m1FaucetRequest = async (address : string) => {
+  const m1FaucetRequest = async (address: string) => {
     return requestFaucetWithGlobalSigner(
       aptosClient,
       faucetClient,
       coinClient,
-      FAUCET_URL,
+      M1_FAUCET_URL,
       address
     );
   };
 
-  const m2FaucetRequest = async (address : string) => {
+  const m2FaucetRequest = async (address: string) => {
     return m2RequestFaucet(
       M2_URL,
       address
     )
   };
 
-  const handleM1evmFaucetRequest = async (address : string) => {
+  const handleM1evmFaucetRequest = async (address: string) => {
     return mevmRequestFaucet(
-      MEVM_URL,
+      MEVM_M1_URL,
       address
     )
   };
 
   return (
     <>
-   <Chain name="M1" amount={10} hasEvm={true} faucetRequest={m1FaucetRequest} evmRequest={handleM1evmFaucetRequest} />
-   <Chain name="M2" amount={1000} hasEvm={false} faucetRequest={m2FaucetRequest} evmRequest={handleM1evmFaucetRequest} />
-   </>
+      <Chain name="M1" amount={10} hasEvm={true} faucetRequest={m1FaucetRequest} evmRequest={handleM1evmFaucetRequest} />
+      <Chain name="M2" amount={1000} hasEvm={false} faucetRequest={m2FaucetRequest} evmRequest={handleM1evmFaucetRequest} />
+    </>
   );
 }
