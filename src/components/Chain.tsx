a4dbf5eb97487205ad5a39e65bac96aec667bbc7
primata <primata@movementlabs.xyz>
2024-07-31 20:38:46 -0300

    add complete directory

--- src/components/Chain.tsx
+++ src/components/Chain.tsx
@@ -49,9 +49,9 @@ export default function Chains({ name,eventName, language, amount, isEvm, networ
     
     const checkRateLimit = async () => {
         try {
-          const response = await fetch('/api/rate-limit');
+          const response = await fetch('/src/pages/api/rate-limit');
           console.log("Response:", response);
-          
+
           // Check if the response is an HTML page
           const contentType = response.headers.get('content-type');
           if (!contentType || !contentType.includes('application/json')) {
