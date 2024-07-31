c8e4e0bcab31e277000e0bb6ae347065dd2dd783
primata <primata@movementlabs.xyz>
2024-07-31 20:37:52 -0300

    rename

--- src/pages/api/rate-limit.ts
+++ src/pages/api/rate-limit.ts
@@ -1,50 +1,25 @@
-import { ipAddress } from '@vercel/functions';
+import { NextRequest, NextResponse } from 'next/server';
 import { Ratelimit } from '@upstash/ratelimit';
 import { kv } from '@vercel/kv';
-import { NextResponse } from 'next/server';
 
 const ratelimit = new Ratelimit({
   redis: kv,
-  // 3 requests from the same IP in 30 seconds
-  limiter: Ratelimit.slidingWindow(3, '30 s'),
+  // 5 requests from the same IP in 10 seconds
+  limiter: Ratelimit.slidingWindow(2, '10 s'),
 });
 
 // Define which routes you want to rate limit
 export const config = {
-  runtime: 'edge',
+  matcher: '/',
 };
 
-export default async function GET(request: Request) {
-  try {
-    const ip = ipAddress(request) || '127.0.0.1';
-    console.log(`IP Address: ${ip}`);
-    const { success, pending, limit, reset, remaining } = await ratelimit.limit(ip);
-
-    console.log({
-      success,
-      limit,
-      reset,
-      remaining,
-    });
-
-    return new NextResponse(JSON.stringify({
-      success,
-      limit,
-      reset,
-      remaining,
-    }), {
-      status: success ? 200 : 429,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  } catch (error) {
-    console.error('Error in handler:', error);
-    return new NextResponse(JSON.stringify({ error: 'Internal Server Error' }), {
-      status: 500,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  }
+export default async function handler(request: NextRequest) {
+  // You could alternatively limit based on user ID or similar
+  const ip = request.ip ?? '127.0.0.1';
+  const { success, pending, limit, reset, remaining } = await ratelimit.limit(
+    ip
+  );
+  return success
+    ? NextResponse.next()
+    : NextResponse.redirect(new URL('/blocked', request.url));
 }
