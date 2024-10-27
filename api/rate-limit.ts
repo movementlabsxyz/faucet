089dee2dff4cd78a2c816c6838a57b2ebf526446
primata <primata@movementlabs.xyz>
2024-10-27 17:43:09 -0300

    add rate limit

--- api/rate-limit.ts
+++ api/rate-limit.ts
@@ -7,7 +7,7 @@ import {Aptos, AptosConfig, Network} from "@aptos-labs/ts-sdk";
 const ratelimit = new Ratelimit({
   redis: kv,
   // 3 requests from the same IP in 24 hours
-  limiter: Ratelimit.slidingWindow(2, "86400 s"),
+  limiter: Ratelimit.slidingWindow(2, "60 s"),
 });
 
 type ExtendedIncomingMessage = IncomingMessage & {
@@ -26,62 +26,6 @@ function ips(req: any) {
   return getXForwardedFor(req)?.split(/\s*,\s*/);
 }
 
-// TODO: Use createAssessment instead.
-async function createAssessment(
-  // TODO: Replace the token and reCAPTCHA action variables before running the sample.
-  projectID: string,
-  recaptchaKey: string,
-  recaptchaAction: string,
-  token: string,
-) {
-  // Create the reCAPTCHA client.
-  // TODO: Cache the client generation code (recommended) or call client.close() before exiting the method.
-  const client = new RecaptchaEnterpriseServiceClient();
-  const projectPath = client.projectPath(projectID);
-
-  // Build the assessment request.
-  const request = {
-    assessment: {
-      event: {
-        token: token,
-        siteKey: recaptchaKey,
-      },
-    },
-    parent: projectPath,
-  };
-  console.log("creating assessment");
-  const [response] = await client.createAssessment(request);
-  console.log("response", response);
-  if (!response.tokenProperties) return null;
-  // Check if the token is valid.
-  if (!response.tokenProperties.valid) {
-    console.log(
-      `The CreateAssessment call failed because the token was: ${response.tokenProperties.invalidReason}`,
-    );
-    return null;
-  }
-
-  // Check if the expected action was executed.
-  // The `action` property is set by user client in the grecaptcha.enterprise.execute() method.
-  if (response.tokenProperties.action === recaptchaAction) {
-    // Get the risk score and the reason(s).
-    // For more information on interpreting the assessment, see:
-    // https://cloud.google.com/recaptcha-enterprise/docs/interpret-assessment
-    if (!response.riskAnalysis) return null;
-    console.log(`The reCAPTCHA score is: ${response.riskAnalysis.score}`);
-    response.riskAnalysis.reasons?.forEach((reason) => {
-      console.log(reason);
-    });
-
-    return response.riskAnalysis.score;
-  } else {
-    console.log(
-      "The action attribute in your reCAPTCHA tag does not match the action you are expecting to score",
-    );
-    return null;
-  }
-}
-
 export default async function handler(request: any, response: any) {
   // You could alternatively limit based on user ID or similar
   const {token, address, network} = request.body;
@@ -92,33 +36,24 @@ export default async function handler(request: any, response: any) {
     console.log(`secret key not set`);
     return request.status(500).json({error: "reCAPTCHA secret key not set"});
   }
-  // const ip = ips(request) ?? "127.0.0.1";
+  const ip = ips(request) ?? "127.0.0.1";
 
-  // const score = await createAssessment("movement-faucet-1722352143785", "6LdVjR0qAAAAAFSjzYqyRFsnUDn-iRrzQmv0nnp3", "", token);
-  // console.log('score', score)
-  // if (score === (null || undefined)) {
-  //   response.status(400).json({ error: 'Invalid reCAPTCHA token' });
-  // } else if (score != null && score < 0.5) {
-  //   response.status(400).json({ error: 'reCAPTCHA score too low' });
-  // } else {
-  //   response.status(success ? 200 : 429).json({ success, pending, limit, reset, remaining });
-  // }
-  // const {success, pending, limit, reset, remaining} = await ratelimit.limit(
-  //   ip[0],
-  // );
-  // const {success : addressSuccess, pending: addressPending, limit: addressLimit, reset: addressReset, remaining: addressRemaining} = await ratelimit.limit(
-  //   address,
-  // );
+  const {success, pending, limit, reset, remaining} = await ratelimit.limit(
+    ip[0],
+  );
+  const {success : addressSuccess, pending: addressPending, limit: addressLimit, reset: addressReset, remaining: addressRemaining} = await ratelimit.limit(
+    address,
+  );
   
-  // if (!success) {
-  //   console.log(`ip rate limit`);
-  //   return response.status(429).json({success: false, error: "IP rate limited"});
-  // }
+  if (!success) {
+    console.log(`ip rate limit`);
+    return response.status(429).json({success: false, error: "IP rate limited"});
+  }
 
-  // if (!addressSuccess) {
-  //   console.log(`address rate limit`);
-  //   return response.status(429).json({success: false, error: "Address rate limited"});
-  // }
+  if (!addressSuccess) {
+    console.log(`address rate limit`);
+    return response.status(429).json({success: false, error: "Address rate limited"});
+  }
   
   const verification = await fetch(verificationUrl, {method: "POST"});
   try {
